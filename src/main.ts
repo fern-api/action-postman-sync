2576813800b74fed412bfbc7977d91b0a985c460
Deep Singhvi <deep@usebirch.com>
2022-10-30 16:57:35 -0700

    v0 impl (#8)
    * implement v0 action
    
    * fix lint and build
    
    * build
    
    * fix test
    
    * try with fake inputs
    
    * setup real test
    
    * json stringify error
    
    * rebuild dist directory
    
    * pass secrets in directly
    
    * use petstore collection.json
    
    * example uses 2.1 collection schema
    
    * update readme

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,120 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core";
+import { Collection, CollectionDefinition } from "postman-collection";
+import { FernPostmanClient } from "@fern-fern/postman-sdk";
+import { PostmanCollectionSchema } from "@fern-fern/postman-sdk/resources";
+import { readFile } from "fs/promises";
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    try {
+        const postmanApiKey: string = core.getInput("api-key");
+        const postmanWorkspaceId: string = core.getInput("workspace-id");
+        const postmanCollectionPath: string = core.getInput("collection-path");
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+        const postmanCollection = JSON.parse(
+            (await readFile(postmanCollectionPath)).toString()
+        ) as PostmanCollectionSchema;
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+        const postmanClient = new FernPostmanClient({
+            auth: {
+                apiKey: postmanApiKey,
+            },
+        });
+
+        const getWorkspaceResponse = await postmanClient.workspace.getWorkspace(
+            {
+                workspaceId: postmanWorkspaceId,
+            }
+        );
+        if (!getWorkspaceResponse.ok) {
+            throw new Error(
+                `Failed to load workspace: ${JSON.stringify(
+                    getWorkspaceResponse.error
+                )}`
+            );
+        }
+
+        const collectionMetadataResponse =
+            await postmanClient.collection.getAllCollectionMetadata({
+                workspace: postmanWorkspaceId,
+            });
+        if (!collectionMetadataResponse.ok) {
+            throw new Error(
+                `Failed to load collection metadata from workspace: ${JSON.stringify(
+                    collectionMetadataResponse.error
+                )}`
+            );
+        }
+        const collectionMetadata =
+            collectionMetadataResponse.body.collections.find(
+                (collectionMetadataItem) => {
+                    collectionMetadataItem.name === postmanCollection.info.name;
+                }
+            );
+        const collectionDefinition: CollectionDefinition = {
+            ...postmanCollection,
+            auth:
+                postmanCollection.auth != null
+                    ? postmanCollection.auth._visit({
+                          basic: () => {
+                              return { ...postmanCollection.auth };
+                          },
+                          bearer: () => {
+                              return { ...postmanCollection.auth };
+                          },
+                          _other: () => undefined,
+                      })
+                    : undefined,
+        };
+        if (collectionMetadata != null) {
+            const updateCollectionResponse =
+                await postmanClient.collection.updateCollection({
+                    collectionId: collectionMetadata.uid,
+                    _body: {
+                        collection: new Collection(collectionDefinition),
+                    },
+                });
+            if (!updateCollectionResponse.ok) {
+                throw new Error(
+                    `Failed to update collection in workspace ${
+                        getWorkspaceResponse.body.name
+                    }. 
+                    ${JSON.stringify(
+                        updateCollectionResponse.error,
+                        undefined,
+                        2
+                    )}`
+                );
+            }
+            core.info(
+                `Successfully updated collection in workspace ${getWorkspaceResponse.body.name}!`
+            );
+        } else {
+            const createCollectionResponse =
+                await postmanClient.collection.createCollection({
+                    workspace: postmanWorkspaceId,
+                    _body: {
+                        collection: new Collection(collectionDefinition),
+                    },
+                });
+            if (!createCollectionResponse.ok) {
+                throw new Error(
+                    `Failed to create collection in workspace ${
+                        getWorkspaceResponse.body.name
+                    }. 
+                    ${JSON.stringify(
+                        createCollectionResponse.error,
+                        undefined,
+                        2
+                    )}`
+                );
+            }
+            core.info(
+                `Successfully created collection in workspace ${getWorkspaceResponse.body.name}!`
+            );
+        }
+    } catch (error) {
+        if (error instanceof Error) core.setFailed(error.message);
+    }
 }
 
-run()
+run();
